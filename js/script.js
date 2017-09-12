a115bb83c3c71f9198514f0fd657db54debe8f4a
Safe_Mode <safe2mode@gmail.com>
2017-09-13 00:08:41 +0300

    Тени нарисовал для попапов

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var link = document.querySelector(".contacts__button");
 
 var popup = document.querySelector(".modal__contact");
 var close = popup.querySelector(".modal__close");
-var overlay = document.querySelector(".modal__overlay");
+var shadow = document.querySelector(".modal__shadow");
+// var overlay = document.querySelector(".modal__overlay");
 
 var form = popup.querySelector("form");
 var name = popup.querySelector("[name=name]");
@@ -14,7 +15,8 @@ var storage = localStorage.getItem("name");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal--show-anim");
-  overlay.classList.add("modal--show");
+  shadow.classList.add("modal--show-anim");
+  // overlay.classList.add("modal--show");
 
   if (storage) {
     name.value = storage;
@@ -27,33 +29,35 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal--show-anim");
-  overlay.classList.remove("modal--show");
+  shadow.classList.remove("modal--show-anim");
+  // overlay.classList.remove("modal--show");
   popup.classList.remove("modal--error");
 });
 
-overlay.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal--show-anim");
-  overlay.classList.remove("modal--show");
-  popup.classList.remove("modal--error");
-});
-
-form.addEventListener("submit", function (evt) {
-  if (!name.value || !comment.value) {
-    evt.preventDefault();
-    popup.classList.remove("modal--error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal--error");
-  } else {
-    localStorage.setItem("name", name.value);
-  }
-});
+// overlay.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   popup.classList.remove("modal--show-anim");
+//   overlay.classList.remove("modal--show");
+//   popup.classList.remove("modal--error");
+// });
+//
+// form.addEventListener("submit", function (evt) {
+//   if (!name.value || !comment.value) {
+//     evt.preventDefault();
+//     popup.classList.remove("modal--error");
+//     popup.offsetWidth = popup.offsetWidth;
+//     popup.classList.add("modal--error");
+//   } else {
+//     localStorage.setItem("name", name.value);
+//   }
+// });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("modal--show-anim")) {
       popup.classList.remove("modal--show-anim");
-      overlay.classList.remove("modal--show");
+      shadow.classList.remove("modal--show-anim");
+      // overlay.classList.remove("modal--show");
       popup.classList.remove("modal--error");
     }
   }
@@ -68,26 +72,27 @@ var mapLink = document.querySelector(".contacts__map");
   mapLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     mapPopup.classList.add("modal--show-anim");
-    overlay.classList.add("modal--show");
+    // overlay.classList.add("modal--show");
   });
 
   mapClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     mapPopup.classList.remove("modal--show-anim");
-    overlay.classList.remove("modal--show");
+    // overlay.classList.remove("modal--show");
   });
 
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       if (mapPopup.classList.contains("modal--show-anim")) {
         mapPopup.classList.remove("modal--show-anim");
-        overlay.classList.remove("modal--show");
+        shadow.classList.remove("modal--show-anim");
+        // overlay.classList.remove("modal--show");
       }
     }
   });
 
-  overlay.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mapPopup.classList.remove("modal--show-anim");
-    overlay.classList.remove("modal--show");
-  });
+  // overlay.addEventListener("click", function (evt) {
+  //   evt.preventDefault();
+  //   mapPopup.classList.remove("modal--show-anim");
+  //   overlay.classList.remove("modal--show");
+  // });
